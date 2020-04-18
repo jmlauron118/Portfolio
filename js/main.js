40b8e02102be3912a3033fdb87744f6fa8ca0ea7
jmlauron118 <janmarklauron@gmail.com>
2020-04-18 11:41:46 +0800

    commit[041820 - 1141am]

--- js/main.js
+++ js/main.js
@@ -20,28 +20,67 @@ function Portfolio() {
         _portfolio.StickyMenu(navbar, sticky);
         _portfolio.UpdateAge();
         _portfolio.MenuClickEvent();
+        _portfolio.HoverEvent();
 
         document.getElementById("logo").addEventListener("click", () => {
             var url = window.location.href;
-            var homeUrl = url.split("index.html")[0] + "index.html";
-            console.log(homeUrl);
+            var homeUrl = url.includes("index.html") ? url.split("index.html")[0] + "index.html" : url + "index.html";
             window.location.replace(homeUrl);
         });
     });
 
     this.MenuClickEvent = (() => {
-        var allMenu = document.querySelectorAll("a[class='nav-link']");
+        var allMenu = document.querySelectorAll("a[class^=nav-link]");
 
         for (var i = 0; i < allMenu.length; i++) {
             allMenu[i].addEventListener("click", function () {
-                var test = Array.from(document.querySelectorAll("a[class='nav-link active']"));
-                for(let elem of test){
+                var a = Array.from(document.querySelectorAll("a[class^='nav-link']"));
+
+                for (let elem of a) {
                     elem.classList.remove("active");
                 }
 
-                this.classList.add("active");
+                this.classList.add("active"); 
+
+                var id = this.id;
+                var parentId = id.split("-")[1];
+                var parentDiv = document.getElementById(parentId);
+                var topPos = parentDiv.offsetTop;
+
+                document.getElementsByTagName("BODY")[0].scrollTop = topPos-100;
             });
         }
+    });
+
+    this.HoverEvent = (() => {
+        var section = document.querySelectorAll("div[class='info-content']");
+
+        window.addEventListener("scroll", function () {
+            var currentScroll = window.pageYOffset;
+            var currentSection;
+
+            for (var i = 0; i < section.length; i++) {
+                var divPosition = section[i].offsetTop - 200;
+
+                if (divPosition - 1 < currentScroll) {
+                    currentSection = section[i];
+
+                    var id = currentSection.id;
+                    var navLink = document.querySelector("a[id='nav-" + id + "']");
+                    var a = Array.from(document.querySelectorAll("a[class='nav-link active']"));
+
+                    if (a != undefined) {
+                        for (let elem of a) {
+                            elem.classList.remove("active");
+                        }
+                    }
+
+                    if (navLink != null) {
+                        navLink.classList.add("active");
+                    }
+                } 
+            }
+        });
     });
 
     this.UpdateAge = (() => {
