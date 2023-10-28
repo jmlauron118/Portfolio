577de006e3d78fc11e1352ce3f957e2175b97a16
Jan Mark Lauron <janmarklauron@gmail.com>
2023-10-29 02:35:34 +0800

    [Commit] - Update(CV file, and deetails)

--- content/js/main.js
+++ content/js/main.js
@@ -46,6 +46,7 @@ function Portfolio() {
     _portfolio.UpdateAge();
     _portfolio.MenuClickEvent();
     _portfolio.HoverEvent();
+    _portfolio.CalcYrExp();
 
     document.getElementById("logo").addEventListener("click", () => {
       var url = window.location.href;
@@ -191,6 +192,13 @@ function Portfolio() {
       }
     }
   };
+
+  this.CalcYrExp = () =>{
+    var diffMs = new Date() - new Date("2018-12-29");
+    var totalExpDt = new Date(diffMs);
+    
+    document.getElementById("lblExp").innerHTML = Math.abs(totalExpDt.getUTCFullYear() - 1970) + "+";
+  }
 
   function showHideElem(elem, isShown) {
     if (isShown) {
